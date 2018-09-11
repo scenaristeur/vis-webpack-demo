c893aeb1d4d62803eb8592a47cdd12acb1c186bc
dfaveris <scenaristeur@gmail.com>
2018-09-11 13:14:11 +0200

    add poly

--- webpack.config.js
+++ webpack.config.js
@@ -13,57 +13,65 @@ var config = {
   },
   plugins: [
     /* prevent that webpack loads momentjs-support for all languages. Only DE and EN.
-     * see http://stackoverflow.com/questions/25384360/how-to-prevent-moment-js-from-loading-locales-with-webpack
-     */
+    * see http://stackoverflow.com/questions/25384360/how-to-prevent-moment-js-from-loading-locales-with-webpack
+    */
     new webpack.ContextReplacementPlugin(/moment[\\\/]locale$/, /^\.\/(de|en)$/),
   ],
   module: {
     rules: [{
-        test: /node_modules[\\\/]vis[\\\/].*\.js$/, // vis.js files
-        loader: 'babel-loader',
-        query: {
-          cacheDirectory: true,
-          presets: [ "babel-preset-es2015" ].map(require.resolve),
-          plugins: [
-            "transform-es3-property-literals", // see https://github.com/almende/vis/pull/2452
-            "transform-es3-member-expression-literals", // see https://github.com/almende/vis/pull/2566
-            "transform-runtime" // see https://github.com/almende/vis/pull/2566
-          ]
-        }
-      }, {
-        test: /\.js$/, //Check for all js files
-        loader: 'babel-loader',
-        query: {
-          presets: [ "babel-preset-es2015" ].map(require.resolve)
-        }
-      }, {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
+      test: /node_modules[\\\/]vis[\\\/].*\.js$/, // vis.js files
+      loader: 'babel-loader',
+      query: {
+        cacheDirectory: true,
+        presets: [ "babel-preset-es2015" ].map(require.resolve),
+        plugins: [
+          "transform-es3-property-literals", // see https://github.com/almende/vis/pull/2452
+          "transform-es3-member-expression-literals", // see https://github.com/almende/vis/pull/2566
+          "transform-runtime" // see https://github.com/almende/vis/pull/2566
         ]
-      }, {
-        test: /.*\.png$/i,
-        loaders: [ 'file-loader', {
-          loader: 'image-webpack-loader',
-          query: {
-            progressive: true,
-            pngquant: {
-              quality: '65-90',
-              speed: 4
-            }
-          }
-        }
+      }
+    }, {
+      test: /\.js$/, //Check for all js files
+      loader: 'babel-loader',
+      query: {
+        presets: [ "babel-preset-es2015" ].map(require.resolve)
+      }
+    }, {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
       ]
+    }, {
+      test: /.*\.png$/i,
+      loaders: [ 'file-loader', {
+        loader: 'image-webpack-loader',
+        query: {
+          mozjpeg: {
+            progressive: true,
+          },
+          gifsicle: {
+            interlaced: false,
+          },
+          optipng: {
+            optimizationLevel: 4,
+          },
+          pngquant: {
+            quality: '65-90',
+            speed: 4,
+          },
+        },
       }
     ]
-  },
-  //To run development server
-  devServer: {
-    contentBase: __dirname + '/src',
-  },
+  }
+]
+},
+//To run development server
+devServer: {
+  contentBase: __dirname + '/src',
+},
 
-  devtool: "eval-source-map" // Default development sourcemap
+devtool: "eval-source-map" // Default development sourcemap
 };
 
 // Check if build is running in production mode, then change the sourcemap type
